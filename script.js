5e8b49e3c3927b1f88f7527615209a746d6dc0cc
Iris Leite Vieira Junior <irisjunior1@gmail.com>
2021-04-29 14:58:05 -0300

    inclusao de codigos

---
+++ script.js
@@ -0,0 +1,23 @@
+function carregar() {
+  
+  var msg = document.getElementById('msg')
+  var img  = document.getElementById('img')
+  var data = new Date()
+  var hora = 19
+
+  msg.innerHTML = `Agora São ${hora} horas`
+
+  if (hora >=0 && hora < 12){
+    img.src = './img/manha.jpg'
+    document.body.style.backgroundColor = '#e2cd9f'
+  } else if (hora >= 12 && hora <= 18){
+    img.src = './img/tarde.jpg'
+    document.body.style.backgroundColor = '#b9846f'
+  } else { 
+    img.src = './img/noite.jpg'
+    document.body.style.backgroundColor = '#bbbb'
+  }
+
+}
+
+  
